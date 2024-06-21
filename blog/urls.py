8440dcc6dffe15796a1cf266ab442f3f114c9cd6
Veniiti <pauyzsow@gmail.com>
2024-06-21 14:00:36 +0200

    ursl zmiany i importy

--- blog/urls.py
+++ blog/urls.py
@@ -4,3 +4,16 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
+
+# urls.py
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # Twoje URL-e aplikacji
+]
+
+# Dodaj poniższe linie tylko w trybie DEBUG
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
